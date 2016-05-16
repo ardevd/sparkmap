55a677279ca0246c925066be382b58d9519fe8fc
eho <edvard.holst@gmail.com>
2016-05-16 11:19:23 +0200

    Added placeholder CommentsListViewController
    - Will show list of comments, if any, for a given charger.
    
    References #5

---
+++ app/Spark/CommentsListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CommentsListViewController.swift
+//  SparkMap
+//
+//  Created by Edvard Holst on 16/05/16.
+//  Copyright © 2016 Zygote Labs. All rights reserved.
+//
+
+import UIKit
+
+class CommentsListViewController: UIViewController {
+    
+    var charger: ChargerPrimary?
+    var comments: [Comment] = [Comment]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
