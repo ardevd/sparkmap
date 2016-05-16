b2147d79596e3b9076c4f958af25473bf6c0540b
eho <edvard.holst@gmail.com>
2016-05-16 12:50:03 +0200

    Updated CommentsListViewController
    Now loads the tableView and populates it with comments from persistent storage data source.
    
    References #5

--- app/Spark/CommentsListViewController.swift
+++ app/Spark/CommentsListViewController.swift
@@ -8,15 +8,39 @@
 
 import UIKit
 
-class CommentsListViewController: UIViewController {
+class CommentsListViewController: UIViewController, UITableViewDelegate, UINavigationControllerDelegate {
+    
+    // Outlets
+    @IBOutlet var chargerTitleLabel: UILabel!
+    @IBOutlet var chargerCommentsTableView: UITableView!
     
     var charger: ChargerPrimary?
     var comments: [Comment] = [Comment]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Configure TableView
+        let nib = UINib(nibName: "CommentTableViewCell", bundle: nil)
+        chargerCommentsTableView.registerNib(nib, forCellReuseIdentifier: "net.zygotelabs.commentcell")
+        chargerCommentsTableView.tableFooterView = UIView(frame: CGRectZero)
 
         // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return comments.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("net.zygotelabs.commentcell", forIndexPath: indexPath) as! CommentTableViewCell
+        
+        cell.commentTextLabel?.text = (comments[indexPath.row] as Comment).comment
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
