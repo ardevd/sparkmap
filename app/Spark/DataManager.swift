dce7e98b9a1f4087a302ff861fefa305cd377359
eho <edvard.holst@gmail.com>
2016-06-28 00:09:07 +0200

    Updated maxresults to 500.

--- app/Spark/DataManager.swift
+++ app/Spark/DataManager.swift
@@ -235,7 +235,7 @@ class DataManager: NSObject {
         
         if (!offlineMode){
             // TODO - Add option to select units of measurements
-            guard let url = NSURL(string: "https://api.openchargemap.io/v2/poi/?output=json&verbose=false&maxresults=400&includecomments=true&distanceunit=KM&latitude=\(latitude)&longitude=\(longitude)") else { return }
+            guard let url = NSURL(string: "https://api.openchargemap.io/v2/poi/?output=json&verbose=false&maxresults=500&includecomments=true&distanceunit=KM&latitude=\(latitude)&longitude=\(longitude)") else { return }
             
             // HTTP GET
             let urlRequest = NSURLRequest(URL: url)
