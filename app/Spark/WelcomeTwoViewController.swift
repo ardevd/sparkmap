1bd08c99c9078517e9609e83c42152f12bb2fb63
eho <edvard.holst@gmail.com>
2016-07-13 19:23:17 +0200

    Designed the second welcome screen.
    References #5

--- app/Spark/WelcomeTwoViewController.swift
+++ app/Spark/WelcomeTwoViewController.swift
@@ -22,14 +22,9 @@ class WelcomeTwoViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func doneButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
 
 }
