1114561113bdcd4aa7dea03b2d2b7718b1babfaf
eho <edvard.holst@gmail.com>
2016-07-22 20:19:35 +0200

    Added CommentComposerViewController. Updated project properties.
    References #19

---
+++ app/Spark/CommentComposerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CommentComposerViewController.swift
+//  SparkMap
+//
+//  Created by Edvard Holst on 22/07/16.
+//  Copyright © 2016 Zygote Labs. All rights reserved.
+//
+
+import UIKit
+
+class CommentComposerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
