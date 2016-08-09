ef674fd8488f5333b5bd7054fabc89e213912902
eho <edvard.holst@gmail.com>
2016-08-08 18:03:00 -0700

    Added a user manager helper class for user profile data.
    - Stores username, reputation, email address, etc.

---
+++ app/Spark/UserManager.swift
@@ -0,0 +1,48 @@
+//
+//  UserManager.swift
+//  SparkMap
+//
+//  Created by Edvard Holst on 08/08/16.
+//  Copyright © 2016 Zygote Labs. All rights reserved.
+//
+
+import Foundation
+
+class UserManager {
+    
+    var username: String?
+    var reputation: Int?
+    var emailAddress: String?
+    var location: String?
+    
+    
+    func storeUserData() {
+        // Store user data.
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        if let ocmUsername = username {
+            defaults.setObject(ocmUsername, forKey: "ocmUsername")
+        }
+        
+        if let ocmReputation = reputation {
+            defaults.setInteger(ocmReputation, forKey: "ocmReputation")
+        }
+        
+        if let ocmEmailAddress  = emailAddress {
+            defaults.setObject(ocmEmailAddress, forKey: "ocmEmailAddress")
+        }
+        
+        if let ocmLocation = location {
+            defaults.setObject(ocmLocation, forKey: "ocmLocation")
+        }
+    }
+    
+    func loadUserDataFromUserDefaults() {
+        // Load stored user data
+        let defaults = NSUserDefaults.standardUserDefaults()
+        username = defaults.objectForKey("ocmUsername") as? String
+        reputation = defaults.integerForKey("ocmReputation")
+        emailAddress = defaults.objectForKey("ocmEmailAddress") as? String
+        location = defaults.objectForKey("ocmLocation") as? String
+    }
+}
